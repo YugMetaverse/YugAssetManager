694fd31eeb3d30713e9e9441ba87287d01e43551
Utkarsh Shukla <utk.shukla@gmail.com>
2023-03-28 12:38:42 +0530

    Updated Simple UGC Module

--- Plugins/SimpleUGC/Source/UGCExample/UGCExample.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UGCExample.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UGCExample, "UGCExample" );
